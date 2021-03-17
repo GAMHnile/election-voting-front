20990a9ec71f1f4c4d044a1e3d69b221ff99e3bf
Akndju <davidd.akindoju@gmail.com>
2021-03-17 17:21:56 +0100

    Corrected votePage styling and added submit btn

--- src/components/votePage/votePage.jsx
+++ src/components/votePage/votePage.jsx
@@ -5,81 +5,96 @@ const VotePage = () => {
   return (
     <div>
       <div className="heading">
-        <h1>BENSON IDAHOSA UNIVERSITY</h1>
-        <h1 className="year">2020/2021</h1>
+        <h1 className="school">BENSON IDAHOSA UNIVERSITY</h1>
+        <h2 className="dept">NACOSS</h2>
+        <h3 className="year">2020/2021</h3>
       </div>
       <form>
-        <div className="position">
+        <div>
           <h2 className="post">President</h2>
-          <div>
-            <div className="name">
-              <input type="radio" name="president" id="Accra" />
-              <label htmlFor="Accra">Accra Jeffery</label>
+          <div className="candidates">
+            <div>
+              <div className="name">
+                <input type="radio" name="president" id="Accra" />
+                <label htmlFor="Accra">Accra Jeffery</label>
+              </div>
+              <img
+                src="https://www.realmadrid.com/img/vertical_380px/modric_380x501_20201203095835.jpg"
+                alt="Zlatan"
+              />
             </div>
-            <img
-              src="https://www.realmadrid.com/img/vertical_380px/modric_380x501_20201203095835.jpg"
-              alt="Zlatan"
-            />
-          </div>
-          <div>
-            <div className="name">
-              <input type="radio" name="president" id="Accra" />
-              <label htmlFor="Accra">Esimaje Misan</label>
+            <div>
+              <div className="name">
+                <input type="radio" name="president" id="Accra" />
+                <label htmlFor="Accra">Esimaje Misan</label>
+              </div>
+              <img
+                src="https://resizing.flixster.com/rNjnlJijkuR4JKPt1eTyDUxf-lo=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/316589/316589_v9_bb.jpg"
+                alt="Misan"
+              />
             </div>
-            <img
-              src="https://resizing.flixster.com/rNjnlJijkuR4JKPt1eTyDUxf-lo=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/316589/316589_v9_bb.jpg"
-              alt="Misan"
-            />
           </div>
         </div>
 
-        <div className="position">
+        <div>
           <h2 className="post">Vice-President</h2>
-          <div>
-            <div className="name">
-              <input type="radio" name="vice-president" id="Accra" />
-              <label htmlFor="Accra">Accra Jeffery</label>
+          <div className="candidates">
+            <div>
+              <div className="name">
+                <input type="radio" name="vice-president" id="Accra" />
+                <label htmlFor="Accra">Accra Jeffery</label>
+              </div>
+              <img
+                src="https://www.realmadrid.com/img/vertical_380px/modric_380x501_20201203095835.jpg"
+                alt="Zlatan"
+              />
             </div>
-            <img
-              src="https://www.realmadrid.com/img/vertical_380px/modric_380x501_20201203095835.jpg"
-              alt="Zlatan"
-            />
-          </div>
-          <div>
-            <div className="name">
-              <input type="radio" name="vice-president" id="Misan" />
-              <label htmlFor="Misan">Esimaje Misan</label>
+            <div>
+              <div className="name">
+                <input type="radio" name="vice-president" id="Misan" />
+                <label htmlFor="Misan">Esimaje Misan</label>
+              </div>
+              <img
+                src="https://resizing.flixster.com/rNjnlJijkuR4JKPt1eTyDUxf-lo=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/316589/316589_v9_bb.jpg"
+                alt="Misan"
+              />
             </div>
-            <img
-              src="https://resizing.flixster.com/rNjnlJijkuR4JKPt1eTyDUxf-lo=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/316589/316589_v9_bb.jpg"
-              alt="Misan"
-            />
           </div>
         </div>
 
-        <div className="position">
+        <div>
           <h2 className="post">Secretary</h2>
-          <div>
-            <div className="name">
-              <input type="radio" name="secretary" id="Accra" />
-              <label htmlFor="Accra">Accra Jeffery</label>
+          <div className="candidates">
+            <div>
+              <div className="name">
+                <input type="radio" name="secretary" id="Accra" />
+                <label htmlFor="Accra">Accra Jeffery</label>
+              </div>
+              <img
+                src="https://www.realmadrid.com/img/vertical_380px/modric_380x501_20201203095835.jpg"
+                alt="Zlatan"
+              />
             </div>
-            <img
-              src="https://www.realmadrid.com/img/vertical_380px/modric_380x501_20201203095835.jpg"
-              alt="Zlatan"
-            />
-          </div>
-          <div>
-            <div className="name">
-              <input type="radio" name="secretary" id="Misan" />
-              <label htmlFor="Misan">Esimaje Misan</label>
+            <div>
+              <div className="name">
+                <input type="radio" name="secretary" id="Misan" />
+                <label htmlFor="Misan">Esimaje Misan</label>
+              </div>
+              <img
+                src="https://resizing.flixster.com/rNjnlJijkuR4JKPt1eTyDUxf-lo=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/316589/316589_v9_bb.jpg"
+                alt="Misan"
+              />
             </div>
-            <img
-              src="https://resizing.flixster.com/rNjnlJijkuR4JKPt1eTyDUxf-lo=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/316589/316589_v9_bb.jpg"
-              alt="Misan"
-            />
           </div>
         </div>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            alert('Vote casted. No con change am oo!');
+          }}
+        >
+          Cast Vote
+        </button>
       </form>
     </div>
   );
