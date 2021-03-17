881f43a8d6c2acc8e15122668f7665c1214e3d74
Akndju <davidd.akindoju@gmail.com>
2021-03-17 17:00:26 +0100

    Added votePage

--- src/components/votePage/votePage.jsx
+++ src/components/votePage/votePage.jsx
@@ -9,14 +9,75 @@ const VotePage = () => {
         <h1 className="year">2020/2021</h1>
       </div>
       <form>
-        <div className="president">
+        <div className="position">
+          <h2 className="post">President</h2>
           <div>
-            <h3>Accra</h3>
-            <img src="" alt="" />
+            <div className="name">
+              <input type="radio" name="president" id="Accra" />
+              <label htmlFor="Accra">Accra Jeffery</label>
+            </div>
+            <img
+              src="https://www.realmadrid.com/img/vertical_380px/modric_380x501_20201203095835.jpg"
+              alt="Zlatan"
+            />
           </div>
           <div>
-            <h3>Misan</h3>
-            <img src="" alt="" />
+            <div className="name">
+              <input type="radio" name="president" id="Accra" />
+              <label htmlFor="Accra">Esimaje Misan</label>
+            </div>
+            <img
+              src="https://resizing.flixster.com/rNjnlJijkuR4JKPt1eTyDUxf-lo=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/316589/316589_v9_bb.jpg"
+              alt="Misan"
+            />
+          </div>
+        </div>
+
+        <div className="position">
+          <h2 className="post">Vice-President</h2>
+          <div>
+            <div className="name">
+              <input type="radio" name="vice-president" id="Accra" />
+              <label htmlFor="Accra">Accra Jeffery</label>
+            </div>
+            <img
+              src="https://www.realmadrid.com/img/vertical_380px/modric_380x501_20201203095835.jpg"
+              alt="Zlatan"
+            />
+          </div>
+          <div>
+            <div className="name">
+              <input type="radio" name="vice-president" id="Misan" />
+              <label htmlFor="Misan">Esimaje Misan</label>
+            </div>
+            <img
+              src="https://resizing.flixster.com/rNjnlJijkuR4JKPt1eTyDUxf-lo=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/316589/316589_v9_bb.jpg"
+              alt="Misan"
+            />
+          </div>
+        </div>
+
+        <div className="position">
+          <h2 className="post">Secretary</h2>
+          <div>
+            <div className="name">
+              <input type="radio" name="secretary" id="Accra" />
+              <label htmlFor="Accra">Accra Jeffery</label>
+            </div>
+            <img
+              src="https://www.realmadrid.com/img/vertical_380px/modric_380x501_20201203095835.jpg"
+              alt="Zlatan"
+            />
+          </div>
+          <div>
+            <div className="name">
+              <input type="radio" name="secretary" id="Misan" />
+              <label htmlFor="Misan">Esimaje Misan</label>
+            </div>
+            <img
+              src="https://resizing.flixster.com/rNjnlJijkuR4JKPt1eTyDUxf-lo=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/316589/316589_v9_bb.jpg"
+              alt="Misan"
+            />
           </div>
         </div>
       </form>
